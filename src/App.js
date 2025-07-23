a7538f29726e0bceec7e0347415ef13ded3608b4
bmahe1 <maheshreddy3585@gmail.com>
2025-07-23 12:30:58 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Login Credentials</h1>
+      <p><strong>Username:</strong> admin</p>
+      <p><strong>Password:</strong> admin</p>
+    </div>
+  );
+}
+
+export default App;
